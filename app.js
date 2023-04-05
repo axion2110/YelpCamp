0ebd6e8482bce41014249ef8268ae1e92dd26888
Annette <annettefong2008@gmail.com>
2023-04-05 19:45:19 -0400

    SerializeUser & deserializeUser Added

--- app.js
+++ app.js
@@ -78,6 +78,9 @@ app.use(passport.initialize());
 app.use(passport.session());
 passport.use(new LocalStrategy(User.authenticate()));
 
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
+
 app.use((req, res, next) => {
   res.locals.currentUser = req.user;
   res.locals.success = req.flash("success");
